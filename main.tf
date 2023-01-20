6ae51d601d7ff1add29df9bd182d5cd193044394
diegher-testing <96258518+diegher-testing@users.noreply.github.com>
2023-01-20 13:11:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = "${var.aws_region}"
   assume_role {
       role_arn = "arn:aws:iam::${var.target_account_id}:role/${var.terraform_role}"
+      external_id = "asdasdasd"
     }
-  external_id = "asdasdasd"
+  
 }
 
 data "aws_availability_zones" "available" {}
